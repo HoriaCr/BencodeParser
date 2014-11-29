722d148215ebf86aaa010bc780eeb02f2dfa0122
HoriaCr <cretescu_horia@yahoo.com>
2014-11-29 23:06:03 +0200

    added functionality

--- Parser/list.hpp
+++ Parser/list.hpp
@@ -12,13 +12,61 @@ class List : public BencodeType  {
         std::vector<BencodeType*> data;
 
     public: 
+        List();
 
+        ~List();
+
+        List(const List&);
+
+        List& operator = (const List&);
 
         void push(BencodeType*);
+
+        BencodeType *clone();
 };
 
 
+List::List() {
+
+}
+
+List::~List() {
+    
+    for (size_t i = 0; i < data.size(); i++) {
+        delete data[i];    
+    }
+
+    data.clear();
+}
+
+List::List(const List& other) {
+    data.resize(other.data.size());
+    for (size_t i = 0; i < data.size(); i++) {
+        data[i] = other.data[i]->clone();
+    }
+}
+
+List& List::operator = (const List& other) {
+    if (&other == this) {
+        return *this;
+    }
+    
+    this->~List();
+    
+    data.resize(other.data.size());
+    for (size_t i = 0; i < data.size(); i++) {
+        data[i] = other.data[i]->clone();
+    }
+
+    return *this;
+}
+    
 
 void List::push(BencodeType *value) {
     data.push_back(value);
+}
+
+
+BencodeType* List::clone() {
+    return new List(*this);
 }
